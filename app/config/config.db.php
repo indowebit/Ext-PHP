f6422d8dbb54e300edfad44742ddf55cb3d5e650
indowebit <admin@indowebit.co.cc>
2011-05-28 14:51:19 +0700

    change to mysqlt database for master detail

--- app/config/config.db.php
+++ app/config/config.db.php
@@ -1,7 +1,7 @@
 <?php
  $conf_db = Array();
  $conf_db['db_host'] = 'localhost';
- $conf_db['db_type'] = 'mysql';
+ $conf_db['db_type'] = 'mysqlt';
  $conf_db['db_user'] = 'root';
  $conf_db['db_pwd'] = '';
  $conf_db['db_name'] = 'test';
